b7b923d94d68e20bac537d68b6c841d12969dffb
Balthild <ibalthild@gmail.com>
2018-03-02 22:46:06 +0800

    MongoDB authentication

--- config.example.py
+++ config.example.py
@@ -5,6 +5,9 @@ PORT = 5000
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_DB = 'cantonres'
+MONGO_USERNAME = 'cantonres'
+MONGO_PASSWORD = 'cantonres'
+MONGO_AUTH_DB = 'cantonres'
 
 GOOGLE_CLIENT_ID='xxx.apps.googleusercontent.com'
 GOOGLE_MAP_KEY='xxx'
--- lib/mongo.py
+++ lib/mongo.py
@@ -4,15 +4,21 @@ from pymongo.errors import ConnectionFailure
 import config
 
 
-_client = MongoClient(host=config.MONGO_HOST, port=config.MONGO_PORT)
+_client = MongoClient(
+    host=config.MONGO_HOST,
+    port=config.MONGO_PORT,
+    username=config.MONGO_USERNAME,
+    password=config.MONGO_PASSWORD,
+    authSource=config.MONGO_AUTH_DB,
+)
 _database = _client[config.MONGO_DB]
 
 users = _database['users']
-users.create_index('google_id')
-users.create_index('email')
+users.create_index('google_id', unique=True)
+users.create_index('email', unique=True)
 
 sessions = _database['sessions']
-sessions.create_index('token')
+sessions.create_index('token', unique=True)
 
 
 def db():
