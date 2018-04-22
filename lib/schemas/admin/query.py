626b01a71c608c2bd5efce854dc3ef953abd4b72
Balthild <ibalthild@gmail.com>
2018-04-22 14:00:03 +0800

    Article count

--- lib/schemas/admin/query.py
+++ lib/schemas/admin/query.py
@@ -5,12 +5,22 @@ from lib.schemas.types.article import Article, ArticleStatus
 
 
 class AdminQuery(graphene.ObjectType):
+    total_articles = graphene.Int(
+        status=graphene.Argument(ArticleStatus)
+    )
     list_articles = graphene.Field(
         type=graphene.List(Article),
         count=graphene.Int(default_value=30),
         offset=graphene.Int(default_value=0),
         status=graphene.Argument(ArticleStatus)
     )
+
+    def resolve_total_articles(self, info, status=None):
+        cond = {}
+        if status is not None:
+            cond['status'] = status
+
+        return db().articles.find(cond).count()
 
     def resolve_list_articles(self, info, count, offset, status=None):
         cond = {}
